8f1dd9a02dcbdad8606857192ba59ec2d5f4818d
Shivansh Rastogi <88771326+Shivansh1203@users.noreply.github.com>
2024-04-15 13:21:52 +0530

    push

--- app2.py
+++ app2.py
@@ -253,13 +253,44 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+from PIL import Image
+import requests
+import smtplib
 
-# Function to load CSV files from the directory
+
+
+st.set_page_config(page_title="MQH-Volume_Activity", page_icon=":tada:", layout="wide")
+# ---- HEADER SECTION ----
+with st.container():
+    c1, c2 = st.columns(2)
+    with c1:
+        st.title("MQH")
+        st.write(
+            "Stay Ahead of the Game with MQH."
+        )
+        st.write("MQH is a comprehensive online platform dedicated to providing data visualization and in-depth analysis of major moves in international markets. With a focus on empowering traders and investors, MQH offers a range of tools and resources to delve into market trends, enabling users to scrutinize charts and identify trading opportunities with precision.")
+    with c2:
+          
+            image = Image.open('images/mqh_logo.jpeg')
+
+            resized_image = image.resize((300, 300))
+
+            st.image(resized_image)
+            
+
+
+
+
+
+
+st.sidebar.header('MQH (Volume Based Activity)')
+st.sidebar.header("Select The Parameters:")
+
 def load_csv_files(directory):
     csv_files = [file for file in os.listdir(directory) if file.endswith('.csv')]
     return csv_files
 
-# Function to load the selected CSV file
+
 @st.cache_data
 def load_data(selected_csv):
     df = pd.read_csv(selected_csv)
@@ -289,49 +320,50 @@ def create_candlestick_chart(filtered_df):
                                          high=filtered_df['High'],
                                          low=filtered_df['Low'],
                                          close=filtered_df['Close'])])
+    st.subheader("Candlestick Chart (Note-> Newest Date First in the Chart)")
     fig.update_layout(title='Candlestick Chart',
                       xaxis_title='Index Number According to the Table (Newest Date First)',
                       yaxis_title='Price',
                       xaxis_rangeslider_visible=False)
     st.plotly_chart(fig)
 
-# Streamlit UI
-st.title("Volume Percentage Pie Chart and Candlestick Chart")
 
-# Fetch all CSV files from the current directory
+st.title("Dataset Viewer")
+
+
 csv_files = [file for file in os.listdir('.') if file.endswith('.csv')]
 
-# Dropdown menu to select a CSV file
-selected_csv = st.selectbox("Choose a CSV file", csv_files)
+
+selected_csv = st.sidebar.selectbox("Choose a CSV file:", csv_files)
 
 if selected_csv:
     df = load_data(selected_csv)
 
-    # Separate date and time from Timestamp column
+ 
     df['Date'] = pd.to_datetime(df['Timestamp']).dt.date
     df['Time'] = pd.to_datetime(df['Timestamp']).dt.time
 
-    # Create a new DataFrame with Date, Time, and other previous columns
+
     new_df = df[['Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'VWAP', 'Type', 'Range']]
 
-    # Date and time selection
-    selected_date = st.date_input("Select Date", min_value=new_df['Date'].min(), max_value=new_df['Date'].max(), value=new_df['Date'].min())
-    selected_time = st.time_input("Select Time", value=new_df['Time'].iloc[0])
+ 
+    selected_date = st.sidebar.date_input("Select Date:", min_value=new_df['Date'].min(), max_value=new_df['Date'].max(), value=new_df['Date'].min())
+    selected_time = st.sidebar.time_input("Select Time:", value=new_df['Time'].iloc[0])
 
     custom_date = pd.to_datetime(f"{selected_date.strftime('%Y-%d-%m')}")
 
-    # Filter data based on selected date and time
+  
     filtered_data = new_df[(new_df['Date'] == custom_date) & (new_df['Time'] == selected_time)]
 
-    st.write("Filtered Data:")
+    st.subheader("Filtered Data:")
     st.write(filtered_data)
 
     if not filtered_data.empty:
-        # Calculate percentage of buy and sell volumes
+       
         buy_percentage, sell_percentage, total_volume, buy_volume, sell_volume = calculate_percentage(new_df, selected_time, custom_date)
 
-        if total_volume > 0:  # Ensure that total_volume is non-zero
-            # Create a pie chart
+        if total_volume > 0:  
+           
             labels = ['Buy', 'Sell']
             sizes = [buy_percentage, sell_percentage]
             volumes = [buy_volume, sell_volume]
@@ -341,24 +373,94 @@ if selected_csv:
             # Plot pie chart using Streamlit's native function
             fig1, ax1 = plt.subplots()
             ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90, textprops=dict(color="w"), colors=colors)
-            ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+            ax1.axis('equal')  
 
-            # Show pie chart in Streamlit
+            st.subheader("Buy/Sell Type Volumne Share Pie Chart")
             st.pyplot(fig1)
 
-            # Create custom tooltip format
+            st.subheader("The Required Buy/Sell Volume Percentage upto the selected date for a particular timespan")
+            # tooltip_text = [
+            #     f"Type: {labels[i]}, Volume: {volumes[i]} ({sizes[i]:.2f}%)"
+            #     for i in range(len(labels))
+            # ]
+            # tooltip_text.append(f"Total Volume: {total_volume}")
+
+          
+            # st.write("\n".join(tooltip_text))
             tooltip_text = [
-                f"Type: {labels[i]}, Volume: {volumes[i]} ({sizes[i]:.2f}%)"
+                f"<b>Type:</b> {labels[i]}, <b>Volume:</b> {volumes[i]} ({sizes[i]:.2f}%)"
                 for i in range(len(labels))
             ]
-            tooltip_text.append(f"Total Volume: {total_volume}")
+            tooltip_text.append(f"<b>Total Volume:</b> {total_volume}")
 
             # Display tooltip
-            st.write("\n".join(tooltip_text))
+            st.write("\n".join(tooltip_text), unsafe_allow_html=True)
 
-            # Create candlestick chart
+         
             create_candlestick_chart(new_df)
         else:
             st.write("Total volume is zero. Cannot create pie chart.")
     else:
         st.write("No data available for the selected date and time.")
+
+
+st.sidebar.markdown('''
+---
+Created with ❤️ by [Shivansh](https://github.com/Shivansh1203/Volume_Activity).
+''')
+
+
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Us")
+    st.write("##")
+
+ 
+    def send_email(name, email, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login("epicstruchain@gmail.com", "imttyfmbkriojftd")
+        msg = f"Subject: New message from {name}\n\n{name} ({email}) sent the following message:\n\n{message}"
+        server.sendmail("epicstruchain@gmail.com", "epicstruchain@gmail.com", msg)
+        st.success("Thank you for contacting us.")
+        
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    message = st.text_area("Message")
+
+    if st.button("Send"):
+        send_email(name, email, message)
+
+    
+    st.markdown(
+    """
+    <style>
+       
+         /* Adjust the width of the form elements */
+        .stTextInput {
+            width: 50%;
+        }
+        
+        .stTextArea {
+            width: 20%;
+        }
+        /* Style the submit button */
+        .stButton button {
+            background-color: #45a049;
+            color: #FFFFFF;
+            font-weight: bold;
+            padding: 10px;
+            border-radius: 5px;
+            width: 10%;
+        }
+        /* Style the success message */
+        .stSuccess {
+            color: #0072C6;
+            font-weight: bold;
+            margin-top: 20px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
